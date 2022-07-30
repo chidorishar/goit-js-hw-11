83024399006da4dbfdc3c3dfc64016e3bc6c17c7
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-07-30 23:55:32 +0300

    Added rough query logic & functionality

--- src/js/imagesGallery.js
+++ src/js/imagesGallery.js
@@ -2,29 +2,37 @@ var throttle = require('lodash.throttle');
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// import { fetchCountries } from './fetchImages';
 
 import cardsTmpl from './markup/layouts/cardMarkup.hbs';
 import pixabayAPI from './fetchImagesAPI';
 
 const domEls = {
-  // input: document.querySelector('#search-box'),
-  // countryList: document.querySelector('.country-list'),
-  // countryInfo: document.querySelector('.country-info'),
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('#images-wrapper'),
 };
 
 //in ms
 const THROTTLE_TIMEOUT = 250;
 let gallery = {};
+let pixabayAPIInst = {};
 
 //main function
 (() => {
-  const pixabayAPIInst = new pixabayAPI();
-  // domEls.input.addEventListener('input', throttle(onInput, DEBOUNCE_DELAY));
+  pixabayAPIInst = new pixabayAPI();
   gallery = new SimpleLightbox('.gallery a', { uniqueImages: false });
-  renderImages(testResponse);
+
+  domEls.searchForm.addEventListener('submit', onSearchFormSubmit);
 })();
+
 function renderImages({ hits: images }) {
   domEls.gallery.innerHTML = cardsTmpl({ images });
   gallery.refresh();
+}
+
+function onSearchFormSubmit(event) {
+  event.preventDefault();
+
+  const query = event.target.searchQuery.value;
+
+  pixabayAPIInst.loadImagesByQuery(query).then(renderImages);
 }
